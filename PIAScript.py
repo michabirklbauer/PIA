792728da5f895406dda238c050f5f1335b20ed0f
Micha Birklbauer <micha.birklbauer@gmail.com>
2021-10-11 14:01:53 +0200

    add PIA application draft

---
+++ PIAScript.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+# PIA - APPLICATION
+# 2021 (c) Micha Johannes Birklbauer
+# https://github.com/michabirklbauer/
+# micha.birklbauer@gmail.com
+
+version = "1.0.0"
+date = "20211007"
+
+"""
+DESCRIPTION
+PIAScript is a commandline application to apply PIA without the need of coding
+your own workflows. PIAScript supports several input modes and can be tuned by
+specifying the according arguments.
+
+NOTES - TO BE DELETED!!!
+https://docs.python.org/3/library/argparse.html
+https://docs.python.org/3/howto/argparse.html
+https://www.golinuxcloud.com/python-argparse/
+"""
+
+import argparse
+
+# extract interactions
+
+## input mode 1: pdb codes
+
+## input mode 2: pdb files
+
+## input mode 3: sdf file
+
+# compare interactions
+
+## input mode 1: one sdf file
+
+## input mode 2: two sdf files
+
+# scoring
+
+## input mode 1: one sdf file
+
+## input mode 2: two sdf files
+
+# prediction
+
+## model input (either model file or supply params -> actives.txt, inactives.txt)
+
+## input mode 1: pdb file
+
+## input mode 2: sdf file
